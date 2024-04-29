dd1f569cc27d78a491698bd8e1f17d3c6b87e5ff
Niels Sparre <fynniels12@gmail.com>
2024-04-29 19:48:56 +0200

    updated network generation to use networkx geometric graph

--- playground.py
+++ playground.py
@@ -4,17 +4,26 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
+NETWORKGRID_DIMENSION_X = 300
+NETWORKGRID_DIMENSION_Y = 300
+
 class Node:
-    #skal også have en nodeid?
-    def __init__(self, node_id, rank = 999):
+
+    def __init__(self, node_id, position: tuple[int, int], rank = 999):
         self.node_id = node_id
+        self.pos_x = position[0]
+        self.pox_y = position[1]
         self.rank = rank
 
     def set_rank(self, rank):
         self.rank = rank
 
-    def send_to(asd):
+    def send_to(self, asd):
         pass
+
+    def run(self, env):  # Simpy process
+        while(True):
+            pass
 
 class Connection:
     def __init__(self, from_node, to_node, etx_value = 999):
@@ -23,54 +32,110 @@ class Connection:
         self.etx_value = etx_value
 
 class Network:
-    
-    def __init__(self):
+    # https://networkx.org/documentation/stable/auto_examples/drawing/plot_random_geometric_graph.html 
+
+    def __init__(self, dimensions: tuple[int, int]):
+        # self.dimension_x = dimensions[0]
+        # self.dimension_y = dimensions[1] 
         self.nodes = []
         self.connections = []
 
-    def generate_nodes_and_edges(self, number_of_nodes: int, max_number_of_connection_pr_node: int):
-        for node_id in range(number_of_nodes):
-            self.nodes.append(Node(node_id))
+    def generate_nodes_and_edges(self, number_of_nodes: int, radius: float, seed = None):
 
-        for node in self.nodes:
-            #create connection from individual node to other nodes
-            exclude_con_to = [node.node_id] #exclude connection to itself
-            for _ in range(random.randint(1, max_number_of_connection_pr_node)): #its a network so min connections are 1
-                connection_to = random.choice([x for x in range(number_of_nodes) if x not in exclude_con_to]) # choose at random who the node should have connection to
-                self.connections.append(Connection(node.node_id, connection_to))
-                exclude_con_to.append(connection_to) # update exclude_con_to list as to not create multiple connections to same node later
+        self.networkx_graph = nx.random_geometric_graph(number_of_nodes, radius, seed=seed)
 
-                # OGSÅ HUSK AT GENERATE ETX VÆRDIER TIL CONNECTIONEN
+
+
     
-    def plot(self):
-        # create networkx nodes og edges udfra vores Node og Connection setup
+        # for node_id in range(number_of_nodes):
+        #     self.nodes.append(Node(node_id))
 
-        G = nx.Graph()
+        # for node in self.nodes:
+        #     #create connection from individual node to other nodes
+        #     exclude_con_to = [node.node_id] #exclude connection to itself
+        #     for _ in range(random.randint(1, max_number_of_connection_pr_node)): #its a network so min connections are 1
+        #         connection_to = random.choice([x for x in range(number_of_nodes) if x not in exclude_con_to]) # choose at random who the node should have connection to
+        #         self.connections.append(Connection(node.node_id, connection_to))
+        #         exclude_con_to.append(connection_to) # update exclude_con_to list as to not create multiple connections to same node later
 
-        # translate from our Nodes/Connections setup to networkx nodes and edges
-        for node in self.nodes:
-            G.add_node(node.node_id)
-        for connection in self.connections:
-            G.add_edge(connection.from_node, connection.to_node)
-        
-        nx.draw(G, with_labels = True)
-        plt.show()
-            
+        #         #UPDATE: gnp_random_graph gør vidst basically det jeg har gang i her...
 
+        #         # TODO HUSK AT GENERATE ETX VÆRDIER TIL CONNECTIONEN
+
+
+                
         # for node in self.nodes:
         #     print(node.rank)
         # for connection in self.connections:
         #     print(f"Connection from node: {connection.from_node} to node:{connection.to_node}")
-        
-
-    #SKAL HAVE NOGET TIL AT LAVE NETWORK 
-    # (AKA OGSÅ TEGNE NODES OG EDGES I NETWORKX)
-        
-
-
-
-
     
+    def plot(self):
+        pos = nx.get_node_attributes(self.networkx_graph, "pos") # pos er et dict!
+        nx.draw_networkx_edges(self.networkx_graph, pos)
+        nx.draw_networkx_nodes(self.networkx_graph, pos, node_size=80)
+        plt.show()
+
+
+
+        # G = nx.Graph()
+
+        # # translate from our Nodes/Connections setup to networkx nodes and edges:
+        # for node in self.nodes:
+        #     G.add_node(node.node_id)
+        # for connection in self.connections:
+        #     G.add_edge(connection.from_node, connection.to_node)
+
+        # # TODO NOK OGSÅ PLOT ETX
+
+        # #Draw nodes and edges (Note: this figure does not match the actual x and y values of each node)
+        # nx.draw(G, with_labels = True)
+        # plt.show()
+
+
+            
+
+
+
+def main():
+    print("hello world")
+
+    # Setup simulation
+    pass
+
+    #Vi leder efter Geometric grapghs!
+    # https://networkx.org/documentation/stable/reference/generators.html
+    # HER ER VIGTIG GUIDE: https://networkx.org/documentation/stable/auto_examples/drawing/plot_random_geometric_graph.html 
+
+    #G = nx.binomial_graph(20, 0.2)
+    #G = nx.barabasi_albert_graph(50, 0.2)
+    # n = 40
+    # #pos = {i: (random.gauss(0, 2), random.gauss(0, 2)) for i in range(n)}
+    # G = nx.random_geometric_graph(n, 0.3)
+    # nx.draw(G, with_labels = True)
+    # plt.show()
+
+    # Use seed when creating the graph for reproducibility
+    # G = nx.random_geometric_graph(70, 0.2, seed=896803)
+    # # position is stored as node attribute data for random_geometric_graph
+    # pos = nx.get_node_attributes(G, "pos") # pos er et dict!
+    # nx.draw_networkx_edges(G, pos)
+    # nx.draw_networkx_nodes(G, pos, node_size=80,)
+    # plt.show()
+
+    # nw = Network()
+    # nw.generate_nodes_and_edges(20, 3)
+    # nw.plot()
+
+
+    #UPDATE, VI ER NØDT TIL AT LAVE ET AFSTANDSCONCEPT... ELLERS VIRKER DET SIMPELHEN IKKE ORDENTLIG...
+    # så skal max_number_of_connection_pr_node nok også udskiftet med noget node_signal_range_base hvor jeg så +- noget random for at variabere deres signalstyrke lidt. tænk over hvordan det relatere til ETX
+
+    # Create an environment and start the setup process
+    #env = simpy.Environment()
+    #env.process(test_process(env, "ja taak"))
+
+    # Execute simulation
+    #env.run()
 
 # class testy:
 
@@ -90,31 +155,6 @@ class Network:
 #     while True:
 #         yield env.timeout(1000)
 #         print(yapper)
-
-
-
-def main():
-    print("hello world")
-
-    # Setup simulation
-    pass
-
-
-    nw = Network()
-    nw.generate_nodes_and_edges(20, 3)
-    nw.plot()
-
-
-    #UPDATE, VI ER NØDT TIL AT LAVE ET AFSTANDSCONCEPT... ELLERS VIRKER DET SIMPELHEN IKKE ORDENTLIG...
-
-    # Create an environment and start the setup process
-    env = simpy.Environment()
-    #env.process(test_process(env, "ja taak"))
-
-    # Execute simulation
-    #env.run()
-
-
 
 
 
