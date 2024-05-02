6afdda5413ee6f6edb4454f6bc951356674b7a14
Johan Bülow Kviesgaard <71879174+J-BUELOW-K@users.noreply.github.com>
2024-05-02 13:26:34 +0200

    progress?

--- OF0.py
+++ OF0.py
@@ -1,22 +1,4 @@
-# TODO i OF0 "Selection of the Preferred Parent" Step 1, nævner de at en nabo skal overholder alle steps fra 8.2.1 i RPL standarden, før den overhoevedet kan overvejes i OF0.
-
-
-# The Routing Protocol for Low-Power and Lossy Networks (RPL)
-# specification [RFC6550] defines a generic Distance Vector protocol
-# that is adapted to a variety of Low-Power and Lossy Network (LLN)
-# types by the application of specific Objective Functions (OFs).
-
-# A RPL OF states the outcome of the process used by a RPL node to
-# select and optimize routes within a RPL Instance based on the
-# Information Objects available.  As a general concept, an OF is not an
-# algorithm.  For example, outside RPL, "shortest path first" is an OF
-# where the least cost path between two points is derived as an
-# outcome; there are a number of algorithms that can be used to satisfy
-# the OF, of which the well-known Dijkstra algorithm is an example.
-
-# The separation of OFs from the core protocol specification allows RPL
-# to be adapted to meet the different optimization criteria required by
-# the wide range of deployments, applications, and network designs.
+from dodag import Dodag
 
 
 
@@ -33,6 +15,9 @@ MAXIMUM_RANK_STRETCH = 5
 DEFAULT_RANK_FACTOR  = 1
 MINIMUM_RANK_FACTOR  = 1
 MAXIMUM_RANK_FACTOR  = 4
+
+
+DEFAULT_MIN_HOP_RANK_INCREASE = 256
 
 # constant maximum for the Rank
 INFINITE_RANK = 0xffff
@@ -52,51 +37,100 @@ class OF0:
         pass
     
     
-    def compute_rank_increase(self, parent_rank:int=0):
-        # The step_of_rank Sp that is computed for that link is multiplied by
-        # the rank_factor Rf and then possibly stretched by a term Sr that is
-        # less than or equal to the configured stretch_of_rank.  The resulting
-        # rank_increase is added to the Rank of preferred parent R(P) to obtain
-        # that of this node R(N):
-        
-            # R(N) = R(P) + rank_increase where:
+def compute_rank_increase(dodag, parent_rank:int=0):
+    # The step_of_rank Sp that is computed for that link is multiplied by
+    # the rank_factor Rf and then possibly stretched by a term Sr that is
+    # less than or equal to the configured stretch_of_rank.  The resulting
+    # rank_increase is added to the Rank of preferred parent R(P) to obtain
+    # that of this node R(N):
+    
+        # R(N) = R(P) + rank_increase where:
 
-            # rank_increase = (Rf*Sp + Sr) * MinHopRankIncrease
-        
-        rank_increase = (self.rank_factor * self.step_of_rank + self.stretch_of_rank) * self.MinHopRankIncrease
-        if parent_rank+rank_increase > INFINITE_RANK:
-            return INFINITE_RANK
-        else:
-            return parent_rank+rank_increase
+        # rank_increase = (Rf*Sp + Sr) * MinHopRankIncrease
+    
+    rank_increase = (DEFAULT_RANK_FACTOR * DEFAULT_STEP_OF_RANK + DEFAULT_RANK_STRETCH) * dodag.MinHopRankIncrease
+    if parent_rank+rank_increase > INFINITE_RANK:
+        return INFINITE_RANK
+    else:
+        return parent_rank+rank_increase
         
         
         
         ...
         
-    def compute_preferred_parent(self, neighbors:list, parent:dict):
+def compute_preferred_parent(dodag, neighbors:list):
+    
+    # The parent that is selected as the preferred parent is the one that has the lowest rank among all the neighbors that have been heard within the last DIOInterval.
+    for neighbor in neighbors:
+        out = []
+        for i in range(1, len(neighbors)):
+            out.append(compare_parent(neighbor, neighbors[i]))
+            
+    
+    
+    
+    ...
+    
+def compare_parent(parent_1, parent_2):
+    # The parent that is selected as the preferred parent is the one that has the lowest rank among all the neighbors that have been heard within the last DIOInterval.
+    
+    # comparison only make sense within the same RPL instance
+    if parent_1.dodag.dodag_id != parent_2.dodag.dodag_id:
+        raise NotImplementedError("current implementation only support a single RPL instance")
+    
+    # over two grounded DODAG, select the one with the best administrative preference
+    # if (parent_1.dodag.G and parent_2.dodag.G):
+    #     if parent_1.dodag.Prf != parent_2.dodag.Prf:
+    #         return parent_2.dodag.Prf - parent_1.dodag.Prf
         
-        # The parent that is selected as the preferred parent is the one that has the lowest rank among all the neighbors that have been heard within the last DIOInterval.
-        # If there is a tie, the parent with the lowest MAC address is selected.
-        # If the node has no neighbors that have been heard within the last DIOInterval, the node MUST select the DODAG root as the preferred parent.
-        
+        # prefer grounded DODAG over a floating one
+    # if (not parent_1.dodag.G and parent_2.dodag.G):
+    #     return 1  # parent 2
+    # if (not parent_2.dodag.G and parent_1.dodag.G):
+    #     return -1 # parent 1
+    
+    rank1 = compute_rank_increase(parent_1.dodag, parent_1.dodag.rank)
+    rank2 = compute_rank_increase(parent_2.dodag, parent_2.dodag.rank)
 
+    
+    if (parent_1.dodag.DAGRank(rank1) != parent_2.dodag.DAGRank(rank2)):
+        return rank1 - rank2
+    
+    # a preferred parent should stay preferred
+    if parent_1.preferred:
+        return -1
+
+    if parent_2.preferred:
+        return 1
+    
+    # router that has announced a DIO message more recently should be
+    # preferred
+    return parent_2.dodag.last_dio - parent_1.dodag.last_dio
+    ...
         
-        # The node MUST NOT select a parent that is not in the list of neighbors that have been heard within the last DIOInterval.
-        
-        
-        
-        ...
-        
-    def compare_parent(self, parent_1, parent_2):
-        # The parent that is selected as the preferred parent is the one that has the lowest rank among all the neighbors that have been heard within the last DIOInterval.
-        
-        rank1 = parent_1.compute_rank_increase(parent_1["rank"])
-        rank2 = parent_2.compute_rank_increase(parent_2["rank"])
-        
-        if 
-        
-        
-        ...
-        
-    def compute_backup_parent(self):
-        ...
+class node_tester():
+    
+    def __init__(self, dodag) -> None:
+        self.dodag = dodag
+        self.preferred = False
+        self.rank = 0
+        self.G = False
+        self.Prf = 0
+        self.last_dio = 0
+    
+if __name__ == "__main__":
+    
+    
+    d1 = Dodag(dodag_id=1, dodag_version_num=1, is_root=False)
+    n1 = node_tester(d1)
+    d2 = Dodag(dodag_id=1, dodag_version_num=1, is_root=False)
+    n2 = node_tester(d2)
+    d3 = Dodag(dodag_id=1, dodag_version_num=1, is_root=False)
+    n3 = node_tester(d3)
+    
+    
+    n1_neighbors = [n2, n3]
+
+    
+    compute_preferred_parent(n1, n1_neighbors)
+    
